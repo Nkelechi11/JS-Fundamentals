734db166dedd6e9d66afe661262cb41eafa89c38
Adjavon Clement <adjavonkwabena20@gmail.com>
2025-05-24 15:19:24 +0000

    Add script that prints two arguments in 'is' format

---
+++ 4-concat.js
@@ -0,0 +1,7 @@
+// Step 1: Access the two arguments directly
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+// Step 2: Print in the required format
+console.log(arg1 + " is " + arg2);
+
